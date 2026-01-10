b78abd9fce813092daf3139695a941f5d43cab1b
Daniel Amores <danielamoresjimenez@gmail.com>
2026-01-10 13:59:42 +0100

    Initialize commit

---
+++ src/main/java/com/habitame/api/HabitameApiApplication.java
@@ -0,0 +1,13 @@
+package com.habitame.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HabitameApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HabitameApiApplication.class, args);
+    }
+
+}
