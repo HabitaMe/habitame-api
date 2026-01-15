7c1e9d8f3c29a889bb1127f8abadfa7714a71f48
Daniel Amores <danielamoresjimenez@gmail.com>
2026-01-15 23:48:48 +0100

    Añadir CORS para permitir peticiones desde el puerto 4200

--- src/main/java/com/habitame/api/auth/security/SecurityConfig.java
+++ src/main/java/com/habitame/api/auth/security/SecurityConfig.java
@@ -2,6 +2,7 @@ package com.habitame.api.auth.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -15,6 +16,7 @@ public class SecurityConfig {
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 
         http
+                .cors(Customizer.withDefaults())
                 .csrf(csrf -> csrf.disable())
                 .sessionManagement(sm -> sm.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(auth -> auth
