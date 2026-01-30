eb0e6fad5b287540b8274eb6d71227500ff3ac53
Daniel Amores <danielamoresjimenez@gmail.com>
2026-01-30 15:01:56 +0100

    Excluir los endpoints city y country del filtro para poder hacer pruebas con mayor facilidad

--- src/main/java/com/habitame/api/auth/security/SecurityConfig.java
+++ src/main/java/com/habitame/api/auth/security/SecurityConfig.java
@@ -26,6 +26,8 @@ public class SecurityConfig {
                 .sessionManagement(sm -> sm.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/api/auth/**").permitAll()
+                        .requestMatchers("/api/city/**").permitAll()
+                        .requestMatchers("/api/country/**").permitAll()
                         .anyRequest().authenticated()
                 )
                 .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
