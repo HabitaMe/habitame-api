e821e83613c64d01ec254fdb2a4b88609fa0e33a
Daniel Amores <danielamoresjimenez@gmail.com>
2026-02-26 16:20:55 +0100

    Añadir el metodo findByStatus a PropertyReviewRepository

--- src/main/java/com/habitame/api/propertyReview/repository/PropertyReviewRepository.java
+++ src/main/java/com/habitame/api/propertyReview/repository/PropertyReviewRepository.java
@@ -1,7 +1,11 @@
 package com.habitame.api.propertyReview.repository;
 
 import com.habitame.api.propertyReview.entity.PropertyReviewEntity;
+import com.habitame.api.propertyReview.entity.ReviewStatus;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface PropertyReviewRepository extends JpaRepository<PropertyReviewEntity, Integer> {
+    Page<PropertyReviewEntity> findAllByStatus(ReviewStatus status, Pageable pageable);
 }
