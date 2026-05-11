672a86c15075bde0aef6475b2ad1753759467829
Daniel Amores <danielamoresjimenez@gmail.com>
2026-05-11 21:49:15 +0200

    Fix NotBlank invalido en RoomImageRequest para MultipartFile

--- src/main/java/com/habitame/api/roomImage/dto/RoomImageRequest.java
+++ src/main/java/com/habitame/api/roomImage/dto/RoomImageRequest.java
@@ -1,10 +1,9 @@
 package com.habitame.api.roomImage.dto;
 
-import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import org.springframework.web.multipart.MultipartFile;
 
 public record RoomImageRequest (
-    @NotBlank MultipartFile file,
+    @NotNull MultipartFile file,
     @NotNull Boolean isMain
 ) { };
